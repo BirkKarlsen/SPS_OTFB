71d7e0d1327395e9a1e86995e38a60a19e143417
bkarlsen <birk.emil.karlsen-baeck@cern.ch>
2022-01-18 17:31:40 +0100

    Started analysing the potential wells and I/Q-signals.

---
+++ analysis_files/potential_well_and_IQ_plots.py
@@ -0,0 +1,21 @@
+'''
+File to analyse the I/Q-signals and potential wells from simulations and from the measurements.
+
+author: Birk Emil Karlsen-Bæck
+'''
+
+import utility_files.analysis_tools as at
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+
+
+# I/Q-signals -----------------------------------------------------------------
+
+
+
+
+
+
+# Potential Wells -------------------------------------------------------------
